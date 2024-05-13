fefd6b8ef45cc22585055e89ff0df75b063d181b
Yifan <yjin@starbucks.com>
2024-05-13 08:58:13 -0700

    changed the behavior so that I can move mouse meanwhile

--- AFK/AFK.py
+++ AFK/AFK.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import pyautogui as pg
 from alive_progress import alive_bar
@@ -11,24 +12,21 @@ def AFK(duration=1, verbose=None):
         duration (int, optional): Total length of jiggering. Defaults to 1 min.
         verbose (_type_, optional): debug mode. Defaults to None.
     """
-    duration = int(duration) * 60
+    duration = int(duration)  # * 60
     with alive_bar(duration) as bar:
         while duration > 0:
             # print(f"time left: {duration}s")
-            random_combo = random.sample(range(1, 10), 2)
-            move_x = random_combo[0]
-            if random_combo[1] <= duration:
-                move_duration = random_combo[1]
-            else:
-                move_duration = duration
+            move_x = random.sample(range(-3, 3), 1)[0]
+            sleep_duration = random.sample(range(1, 10), 1)[0]
+            if sleep_duration > duration:
+                sleep_duration = duration
+
+            time.sleep(sleep_duration)
 
             if verbose:
-                print(f"moving {move_x}px in {move_duration}s")
-            pg.moveRel(move_x, 0, duration=move_duration)
+                print(f"moving {move_x}px")
+            pg.moveRel(move_x, move_x)
 
-            if duration < move_duration:
-                progress = duration
-            else:
-                progress = move_duration
+            progress = sleep_duration
             duration -= progress
             bar(progress)
