1ead5c77a0ef523bfed1959af494a8a7ed336359
Yifan <yjin@starbucks.com>
2024-06-14 14:11:38 -0700

    updated verison number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="AFK",
-    version="1.0.0",
+    version="1.0.1",
     description="A mouse jigger written in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
