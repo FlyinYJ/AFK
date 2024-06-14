539edba1ca65f391a407b1665987c5dfb46f28bf
Yifan <yjin@starbucks.com>
2024-06-14 14:10:55 -0700

    Added keyboard interuption feature

--- AFK/AFK.py
+++ AFK/AFK.py
@@ -14,20 +14,25 @@ def AFK(duration=1, verbose=None):
     """
     duration = int(duration) * 60
     with alive_bar(duration) as bar:
-        while duration > 0:
-            # print(f"time left: {duration}s")
-            move_x = random.sample(range(-3, 3), 1)[0]
-            sleep_duration = random.sample(range(1, 10), 1)[0]
-            if sleep_duration > duration:
-                sleep_duration = duration
+        try:
+            print("Press Ctrl+C to stop")
+            while duration > 0:
+                # print(f"time left: {duration}s")
+                move_x = random.sample(range(-3, 3), 1)[0]
+                sleep_duration = random.sample(range(1, 10), 1)[0]
+                if sleep_duration > duration:
+                    sleep_duration = duration
 
-            time.sleep(sleep_duration)
+                time.sleep(sleep_duration)
 
-            if verbose:
-                print(f"moving {move_x}px")
+                if verbose:
+                    print(f"moving {move_x}px")
 
-            pg.moveRel(move_x, move_x)
+                pg.moveRel(move_x, move_x)
 
-            progress = sleep_duration
-            duration -= progress
-            bar(progress)
+                progress = sleep_duration
+                duration -= progress
+                bar(progress)
+
+        except KeyboardInterrupt:
+            print("Stopped by user")
